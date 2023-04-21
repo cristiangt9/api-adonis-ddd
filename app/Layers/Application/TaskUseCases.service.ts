18fac6a4140abcfb582e9780dfad4ab96d48ad39
Cristian Gonzalez <cristiangt9@gmail.com>
2023-04-21 16:46:08 -0500

    Use cases definition

---
+++ app/Layers/Application/TaskUseCases.service.ts
@@ -0,0 +1,27 @@
+import { TaskEntity } from '../Domain/Task.entity'
+import { TaskRepository } from '../Domain/Task.repository'
+
+// aqui se implementa la lógica de los casos de uso como servicios
+export class TaskUseCases {
+  constructor(private readonly taskRepository: TaskRepository) {}
+
+  public async getAllTasks(): Promise<TaskEntity[]> {
+    return this.taskRepository.getAll()
+  }
+
+  public async getTaskById(id: number): Promise<TaskEntity | null> {
+    return this.taskRepository.getById(id)
+  }
+
+  public async createTask(task: TaskEntity): Promise<TaskEntity> {
+    return this.taskRepository.create(task)
+  }
+
+  public async updateTask(id: number, task: TaskEntity): Promise<TaskEntity | null> {
+    return this.taskRepository.update(id, task)
+  }
+
+  public async deleteTask(id: number): Promise<boolean> {
+    return this.taskRepository.delete(id)
+  }
+}
